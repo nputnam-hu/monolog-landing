772be04078a81296460ddba4d570ffc06d5f23ab
Noah Putnam <noahputnam@gmail.com>
2024-07-26 15:23:00 -0400

    more

--- src/app/page.js
+++ src/app/page.js
@@ -57,7 +57,8 @@ export default function Home() {
         </h2>
       </div>
       <Section2 />
-      <div style={{ height: 300 }} />
+      <div style={{ height: 300 }} className="desktoponly" />
+      <div style={{ height: 200 }} className="mobileonly" />
       <div
         className="col"
         style={{ maxWidth: 800, margin: "auto", alignItems: "center" }}
